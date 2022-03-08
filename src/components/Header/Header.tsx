4f3c086473262da134b7dfe96b5e817ba6f4d596
Karen Mayumi Hirayama <karenm.hirayama@gmail.com>
2022-03-08 18:52:50 -0300

    Adding icon from mui

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
-import { Avatar, Box, Button } from '@mui/material'
-import logo from '../logo.svg'
+import { Avatar, Box, Typography } from '@mui/material'
+import logo from '../logo.svg';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 
 export const Header = () => {
@@ -8,20 +9,59 @@ export const Header = () => {
             sx={{
                 display: 'flex',
                 flexDirection: 'row',
-                justifyContent: 'space-evenly',
+                justifyContent: 'space-between',
                 alignItems: 'center',
-                backgroundColor: 'white'
+                backgroundColor: 'white',
+                marginTop: 1
             }}
         >
             <img src={logo} alt='Logo' style={{ width: '4.5rem' }} />
-            <Box>
-                <Button>
-                    <Avatar />
-                </Button>
-                <Button>
-                    Logout
-                </Button>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                }}
+            >
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        marginLeft: 2
+                    }}
+                >
+                    <Avatar
+                        src='https://avatars.githubusercontent.com/u/89299893?v=4'
+                    />
+            </Box>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    marginLeft: 2
+                }}
+            >
+                <LogoutIcon
+                    sx={{
+                        fontSize: 32
+                    }}
+                />
+                <Typography
+                    sx={{
+                        cursor: 'pointer',
+                        fontFamily: 'Poppins, sans-serif',
+                        fontSize: 10,
+                        fontWeight: 400
+                    }}
+                >
+                    Sing out
+                </Typography>
             </Box>
         </Box>
+        </Box >
     )
 }
