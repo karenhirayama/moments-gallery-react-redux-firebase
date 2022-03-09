ac8c13c4a5d248bdcc15c7fc54a5ff7a16937445
Karen Mayumi Hirayama <karenm.hirayama@gmail.com>
2022-03-08 22:16:05 -0300

    Adding Menu in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,19 @@
-import { Avatar, Box, Typography } from '@mui/material'
+import { Avatar, Box, ListItemIcon, Menu, MenuItem, Typography } from '@mui/material'
 import logo from '../logo.svg';
 import LogoutIcon from '@mui/icons-material/Logout';
+import { useState } from 'react';
 
 
 export const Header = () => {
+
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
     return (
         <Box
             sx={{
@@ -31,37 +41,46 @@ export const Header = () => {
                         alignItems: 'center',
                         marginLeft: 2
                     }}
+                    onClick={handleClick}
                 >
                     <Avatar
                         src='https://avatars.githubusercontent.com/u/89299893?v=4'
                     />
-            </Box>
-            <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    marginLeft: 2
-                }}
-            >
-                <LogoutIcon
+                </Box>
+                <Box
                     sx={{
-                        fontSize: 32
-                    }}
-                />
-                <Typography
-                    sx={{
-                        cursor: 'pointer',
-                        fontFamily: 'Poppins, sans-serif',
-                        fontSize: 10,
-                        fontWeight: 400
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        marginLeft: 2,
                     }}
                 >
-                    Sing out
-                </Typography>
+                    <Menu
+                        id="demo-positioned-menu"
+                        aria-labelledby="demo-positioned-button"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        anchorOrigin={{
+                            vertical: 100,
+                            horizontal: 'right',
+                        }}
+                        transformOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'right',
+                        }}
+                    >
+
+                        <MenuItem>
+                            <ListItemIcon>
+                                <LogoutIcon fontSize="small" />
+                            </ListItemIcon>
+                            Logout
+                        </MenuItem>
+                    </Menu>
+                </Box>
             </Box>
-        </Box>
         </Box >
     )
 }
