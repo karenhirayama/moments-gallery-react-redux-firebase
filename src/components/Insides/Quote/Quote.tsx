2e2a0d51ff1e28f4d77feea7951b787b5ccab769
Karen Mayumi Hirayama <karenm.hirayama@gmail.com>
2022-03-12 17:01:50 -0300

    Style quote section

--- src/components/Insides/Quote/Quote.tsx
+++ src/components/Insides/Quote/Quote.tsx
@@ -1,11 +1,79 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
+import quoteIcon from './quote-icon.svg';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 
 export const Quote = () => {
+
     return (
-        <Box>
-            <Typography variant='h1'></Typography>
+        <Box
+            sx={{
+                border: '1px solid black',
+                borderRadius: 2,
+                marginLeft: 4,
+                maxWidth: 420,
+            }}
+        >
+            <Box
+                sx={{
+                    marginTop: 3,
+                    marginLeft: 3,
+                    marginRight: 3,
+                }}
+            >
+                <Box>
+                    <img src={quoteIcon} alt='quote icon' />
+                </Box>
+                <Typography
+                    sx={{
+                        marginTop: 1,
+                        marginLeft: 2,
+                        marginRight: 2,
+                        marginBottom: 2,
+                    }}
+                >
+                    Aliqua aliquip anim consequat tempor et. Sunt ut excepteur est deserunt. Tempor eiusmod sunt eu pariatur incididunt consequat. Esse culpa sunt aliqua ex et sit est magna ex reprehenderit aliqua.
+                </Typography>
+            </Box>
+            <Box
+                sx={{
+                    marginLeft: 5,
+                    marginRight: 5,
+                    marginBottom: 2,
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center'
+                }}
+            >
+                <TwitterIcon
+                    sx={{
+                        fontSize: 30,
+                        color: '#4185CA',
+                        cursor: 'pointer',
+                    }}
+                />
+                <Typography
+                    sx={{
+                        border: '1px solid white',
+                        borderRadius: 1,
+                        paddingLeft: 2,
+                        paddingRight: 2,
+                        paddingTop: 0.4,
+                        paddingBottom: 0.4,
+                        backgroundColor: '#79b4fd',
+                        color: 'white',
+                        cursor: 'pointer',
+                        '&:hover': {
+                            color: '#4185CA',
+                            backgroundColor: 'white',
+                            borderColor: '#4185CA'
+                        }
+                    }}
+                >
+                    New quote
+                </Typography>
+            </Box>
         </Box>
     )
 }
