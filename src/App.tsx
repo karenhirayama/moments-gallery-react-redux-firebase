c2d3258630a144c92edde55af477526236cb5dd3
Karen Mayumi Hirayama <karenm.hirayama@gmail.com>
2022-03-13 20:15:17 -0300

    Get user in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,41 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import Feed from './components/Feed/Feed';
 import { Header } from './components/Header/Header';
 import { Insides } from './components/Insides/Insides';
 import { LoginApp } from './components/Login/LoginApp';
+import { login, logout, selectUser } from './features/userSlice';
+import { auth } from './firebase/firebase';
 
 function App() {
 
-  const user = false;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        // User in logged in 
+        dispatch(login({
+          email: userAuth.email,
+          uid: userAuth.uid,
+          displayName: userAuth.displayName,
+          photoURL: userAuth.photoURL
+        }))
+      } else {
+        // User is logged out
+        dispatch(logout())
+      }
+    })
+  })
 
   return (
     <div className="App">
 
       {/* Login */}
 
-      {!user ?
+      {user.user === null  ?
         (
           <>
             <LoginApp />
