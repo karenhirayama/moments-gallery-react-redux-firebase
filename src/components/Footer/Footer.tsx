914578e5b98659b1a87c502304c946426b6914ad
Karen Mayumi Hirayama <karenm.hirayama@gmail.com>
2022-03-16 19:51:17 -0300

    Update Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,29 @@
+import { Box, Link, Typography } from "@mui/material"
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'baseline',
+        justifyContent: 'center'
+      }}
+    >
+      <Typography mr={2}>
+        Create by Karen Mayumi Hirayama
+      </Typography>
+      <FiberManualRecordIcon sx={{fontSize: 10}} />
+      <Link href="https://github.com/karenhirayama/moments-gallery-react-redux-firebase" target='_blank'
+        sx={{
+          textDecoration: 'none',
+          color: 'black',
+          fontFamily: '"Roboto","Helvetica","Arial",sans-serif',
+        }}
+        ml={2}
+      >
+        Link to repository
+      </Link>
+    </Box>
   )
 }
